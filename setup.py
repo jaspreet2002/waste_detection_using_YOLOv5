ec49cf29308c10ec1da5fc0b0ad205b1b3392d62
jaspreet2002 <jaspreet.kaur.csds.20@ggits.net>
2023-08-04 22:31:20 +0530

    final commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'WasteDetection',
+    version= '0.0.0',
+    author= 'Ansh',
+    author_email= 'anshjabalpur@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
